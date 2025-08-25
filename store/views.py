b0fd37396cfe55a3c19f027842e3360c976e3ce5
osman-dev01 <usmanam004@gmail.com>
2025-08-25 03:12:00 +0100

    Add shopping.html and cart.html

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def shopping(request):
+    return render(request, "store/shopping.html")
+
+def cart(request):
+    return render(request, "store/cart.html")
+
+def payment(request):
+    return render(request, "store/payment.html")
